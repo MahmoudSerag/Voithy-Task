7ab929935dffca3b5c5cecdbed649676ebbf8fa7
Mahmoud <sragmahmoud4@gmail.com>
2023-08-09 19:30:58 -0400

    feat: :sparkles: add patient schema

---
+++ backend/database/schemas/patient.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      index: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['doctor', 'patient'],
+    },
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      index: true,
+      ref: 'Doctor',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Patient', PatientSchema);
