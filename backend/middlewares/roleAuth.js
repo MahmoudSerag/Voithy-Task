78769a9e4dfaf1a47b2d2782d93bbc7e6ccbe70c
Mahmoud <sragmahmoud4@gmail.com>
2023-08-09 23:41:34 -0400

    fix: :bug: fix checkRole middleware

--- backend/middlewares/roleAuth.js
+++ backend/middlewares/roleAuth.js
@@ -1,5 +1,6 @@
 const { verifyJWT } = require('../utils/jwtService');
 const asyncHandler = require('../middlewares/async');
+const httpErrors = require('http-errors');
 
 exports.checkRole = asyncHandler(async (req, res, next) => {
   const accessToken = req.cookies.accessToken;
