b263f2823b02274f3d68f260acad80179f8900c7
Mahmoud <sragmahmoud4@gmail.com>
2023-08-10 10:14:19 -0400

    fix: :bug: fix invalid response format for user profile

--- backend/database/models/user.js
+++ backend/database/models/user.js
@@ -9,28 +9,30 @@ exports.getUserProfile = asyncHandler(async (userId, role, page, limit) => {
       .select('-password -role -createdAt -updatedAt -__v')
       .lean();
 
-    const patients = await Patient.find({ doctorId: doctor._id })
-      .select('-password -doctorId -role -createdAt -updatedAt -__v')
+    const patients = await PatientDoctor.find({ doctorId: doctor._id })
+      .select('-doctorId -createdAt -updatedAt -__v')
+      .populate({
+        path: 'patientId',
+        select: 'firstName lastName phoneNumber email',
+        model: Patient,
+      })
       .skip((page - 1) * limit)
       .limit(limit)
       .lean();
 
-    doctor.patients = patients;
+    const patientsInfo = [];
+    patients.forEach((patient) => {
+      delete patient._id;
+      patientsInfo.push(patient.patientId);
+    });
+    doctor.patients = patientsInfo;
 
     return doctor;
   }
 
   const patient = await Patient.findById({ _id: userId })
     .select('-password -role -createdAt -updatedAt -__v')
-    .populate({
-      path: 'doctorId',
-      select: 'firstName lastName phoneNumber email -_id',
-      model: Doctor,
-    })
     .lean();
-
-  patient.doctor = patient.doctorId;
-  delete patient.doctorId;
 
   return patient;
 });
--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_DOMAIN,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowHeaders: [
       'Content-Type',
       'Authorization',
