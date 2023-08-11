4149e3f5d94900166ccd567924d7b9f158fa95e6
Mahmoud <sragmahmoud4@gmail.com>
2023-08-11 14:19:58 -0400

    feat: :sparkles: add production URL in constants.js file

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,7 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://voithy-task.onrender.com';
 
 export const AUTH_URL = '/api/v1/auth';
 export const USERS_URL = '/api/v1/users';
